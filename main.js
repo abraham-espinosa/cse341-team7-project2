5465903c80475e9fca3ce0d19900c4745556b322
etrix06 <alc18005@byui.edu>
2021-10-14 13:12:53 -0700

    added main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+const links = [
+  { label: "Alcala-project341",  url: "https://alcala-project341.herokuapp.com/"  },
+  { label: "CSE341-nodeproject",  url: "https://cse341-nodeproject.herokuapp.com/"  },
+  { label: "project341",  url: "https://project341.herokuapp.com/"  }
+]; 
+
+
+
+function addToIndex() {
+    for (let i = 0; i < links.length; i++ ) {
+      var a = document.createElement("a");
+      var linkText = document.createTextNode(links[i].label);
+      a.appendChild(linkText);
+      a.title = "This is the " + links[i].label + " link";
+      a.href = links[i].url;
+      var y = document.createElement("LI");
+      y.appendChild(a);
+      document.getElementById("indexList").appendChild(y);
+    }
+}
